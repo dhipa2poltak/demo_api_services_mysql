46742fcefc241aa6b2eb3a687054c0eeefbd19a2
Dhipa Poltak <dhipa2poltak@gmail.com>
2025-08-08 19:05:12 +0700

    refactoring code

--- service/VendorService/src/main/java/com/dpfht/demo_api_services_mysql/VendorService/service/VendorService.java
+++ service/VendorService/src/main/java/com/dpfht/demo_api_services_mysql/VendorService/service/VendorService.java
@@ -1,43 +1,14 @@
 package com.dpfht.demo_api_services_mysql.VendorService.service;
 
 import com.dpfht.demo_api_services_mysql.VendorService.model.Vendor;
-import com.dpfht.demo_api_services_mysql.VendorService.repository.VendorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class VendorService {
+public interface VendorService {
 
-    @Autowired
-    private VendorRepository repo;
-
-    //@Override
-    public List<Vendor> findAll() {
-        return repo.findAll();
-    }
-
-    //@Override
-    public Vendor findById(Long id) {
-        return repo.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
-    }
-
-    //@Override
-    public Vendor save(Vendor vendor) {
-        return repo.save(vendor);
-    }
-
-    //@Override
-    public Vendor update(Long id, Vendor vendor) {
-        Vendor existing = findById(id);
-        existing.setName(vendor.getName());
-
-        return repo.save(existing);
-    }
-
-    //@Override
-    public void delete(Long id) {
-        repo.deleteById(id);
-    }
+    public List<Vendor> findAll();
+    public Vendor findById(Long id);
+    public Vendor save(Vendor vendor);
+    public Vendor update(Long id, Vendor vendor);
+    public void delete(Long id);
 }
