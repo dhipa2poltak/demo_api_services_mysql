6a36cbe16d0493444e63086172743caa7f9c8c9c
Dhipa Poltak <dhipa2poltak@gmail.com>
2025-08-08 20:48:21 +0700

    implementing DiscoveryService (Eureka Server)

---
+++ DiscoveryService/src/test/java/com/dpfht/demo_api_services_mysql/DiscoveryService/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpfht.demo_api_services_mysql.DiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
