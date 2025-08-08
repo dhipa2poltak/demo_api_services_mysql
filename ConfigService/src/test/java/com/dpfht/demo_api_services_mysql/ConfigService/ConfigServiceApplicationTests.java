e3d19852dbf0cbda2ac48d2cc1966aa933d99338
Dhipa Poltak <dhipa2poltak@gmail.com>
2025-08-08 21:48:59 +0700

    implementing ConfigService (Config Server)

---
+++ ConfigService/src/test/java/com/dpfht/demo_api_services_mysql/ConfigService/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpfht.demo_api_services_mysql.ConfigService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
